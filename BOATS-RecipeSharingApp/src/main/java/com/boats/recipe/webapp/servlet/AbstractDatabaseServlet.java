4a21bfb1ab237298bf2aa50d432e1b51c8c45b42
maulik225 <msompura00@gmail.com>
2024-04-16 23:01:03 +0200

    Srvlet class created.

---
+++ BOATS-RecipeSharingApp/src/main/java/com/boats/recipe/webapp/servlet/AbstractDatabaseServlet.java
@@ -0,0 +1,61 @@
+package com.boats.recipe.webapp.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.http.HttpServlet;
+import javax.sql.DataSource;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.message.StringFormatterMessageFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class AbstractDatabaseServlet extends HttpServlet {
+    protected static final Logger LOGGER = LogManager.getLogger(AbstractDatabaseServlet.class,
+            StringFormatterMessageFactory.INSTANCE);
+
+    private DataSource ds;
+
+    public void init(ServletConfig config) throws ServletException{
+        InitialContext cxt;
+
+        try {
+            cxt = new InitialContext();
+            ds = (DataSource) cxt.lookup("java:/comp/env/jdbc/boats");
+
+            LOGGER.info("Connection pool to the database successfully acquired.");
+        } catch (NamingException e) {
+            ds = null;
+
+            LOGGER.error("Unable to acquire the connection pool to the database.", e);
+
+            throw new ServletException("Unable to acquire the connection pool to the database", e);
+        }
+
+    }
+    public void destroy() {
+        ds = null;
+        LOGGER.info("Connection pool to the database successfully released.");
+    }
+
+    /**
+     * Returns a {@link  Connection} for accessing the database.
+     *
+     * @return a {@link Connection} for accessing the database
+     *
+     * @throws SQLException if anything goes wrong in obtaining the connection.
+     */
+    protected final Connection getConnection() throws SQLException {
+        try {
+            return ds.getConnection();
+        } catch (final SQLException e) {
+            LOGGER.error("Unable to acquire the connection from the pool.", e);
+            throw e;
+        }
+    }
+}
