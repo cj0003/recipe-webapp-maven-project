6fc30a23364b4bc875572a33b426491995e4c3e0
christymanthara <metalliccJ@gmail.com>
2024-04-26 13:14:01 +0200

    CreateUserRR updated
    

--- BOATS-RecipeSharingApp/src/main/java/com/boats/recipe/webapp/rest/CreateUserRR.java
+++ BOATS-RecipeSharingApp/src/main/java/com/boats/recipe/webapp/rest/CreateUserRR.java
@@ -1,8 +1,10 @@
 package com.boats.recipe.webapp.rest;
 
 import com.boats.recipe.webapp.database.CreateRecipeDAO;
-import com.boats.recipe.webapp.resources.Actions;
+import com.boats.recipe.webapp.database.CreateUserDAO;
 import com.boats.recipe.webapp.model.Recipe;
+import com.boats.recipe.webapp.resources.Actions;
+import com.boats.recipe.webapp.model.User;
 import com.boats.recipe.webapp.resources.LogContext;
 import com.boats.recipe.webapp.resources.Message;
 import javax.servlet.http.HttpServletRequest;
@@ -16,7 +18,7 @@ import java.sql.SQLException;
 public final class CreateUserRR extends AbstractRR {
 
     public CreateUserRR(final HttpServletRequest req, final HttpServletResponse res, final Connection con) {
-        super("CREATE_USER", req, res, con);
+        super(Actions.CREATE_USER, req, res, con);
     }
 
     @Override
@@ -35,6 +37,54 @@ public final class CreateUserRR extends AbstractRR {
         //     res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         //     new Message("Failed to create user account.").toJSON(res.getOutputStream());
         // }
-    }
+        User u = null;
+        Message m = null;
+        try {
+            LOGGER.warn("Inside the try of CreateUserRR.doServe");
+            final User user = User.fromJSON(req.getInputStream());
+            LOGGER.warn("Final user obtained:{}", user.toString());
 
+            LogContext.setResource(Integer.toString(user.getId()));
+
+            // creates a new DAO for accessing the database and stores the user
+            u = new CreateUserDAO(con, user).access().getOutputParam();
+
+            if (u != null) {
+                LOGGER.info("User successfully added.");
+
+                res.setStatus(HttpServletResponse.SC_CREATED);
+                u.toJSON(res.getOutputStream());
+            } else {
+                // it should not happen
+                LOGGER.error("Error encountered while creating User.");
+
+                m = new Message("Cannot create the employee: unexpected error.", "E5A1", null);
+                res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                m.toJSON(res.getOutputStream());
+            }
+
+        } catch (EOFException ex) {
+            LOGGER.warn("Cannot create the User: no User JSON object found in the request.", ex);
+
+            m = new Message("Cannot create the User: no User JSON object found in the request.", "E4A8",
+                    ex.getMessage());
+            res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            m.toJSON(res.getOutputStream());
+        } catch (SQLException sq) {
+            if ("23505".equals(sq.getSQLState())) {
+                LOGGER.warn("Cannot create the User: already exists.");
+
+                m = new Message("Cannot create the User: already exists.", "E5A2", sq.getMessage());
+                res.setStatus(HttpServletResponse.SC_CONFLICT);
+                m.toJSON(res.getOutputStream());
+            } else {
+                LOGGER.error("Cannot create the User: unexpected database error.", sq);
+
+                m = new Message("Cannot create the User: unexpected database error.", "E5A1", sq.getMessage());
+                res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                m.toJSON(res.getOutputStream());
+            }
+        }
+
+    }
 }
